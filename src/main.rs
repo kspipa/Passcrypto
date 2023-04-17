262d33173a9fe5e653bd9407ad0be7234bbc4fe5
kspipa <yyyyyyy5yyyy@gmail.com>
2023-04-17 22:05:04 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 mod aes256;
 mod pass;
 mod file;
+use rpassword;
 fn main(){
     let mut key = vec![0];
     if file::check_file("passs/check".to_string()){
-        if !file::check_file("passs/0.ps".to_string()){
-            println!("0.ps was deleted and your passwords are missed\nWe make another one");
-            file::create_new_ps_file("passs/0.ps".to_string());
-        }
         key = get_pass(false);
         if key[0] == 0{return;}
     }
     else{
         if file::check_file("passs/0.ps".to_string()){
             println!("check was deleted and your master password is missed\nWe make another one");
-            file::create_new_ps_file("passs/0.ps".to_string());
-        }
-        if file::check_dir("passs".to_string()){
-            file::rmdir("passs".to_string());
+            for i in file::get_all_ps("passs".to_string()){
+                file::rmdir("passs/{i}.ps".to_string());
+            }
         }
         key = get_pass(true);
     }
     menu(key);
 }
 fn get_pass(first : bool) -> Vec<u8>{
-    let mut pass = String::new();
     if first{
         file::mkdir("passs".to_string());
         println!("Set your first master password");
-        std::io::stdin().read_line(&mut pass).unwrap();
+        let pass = rpassword::read_password().unwrap();
         let key = pass::get_hash_from_pass(pass.as_bytes());
         file::create_new_ps_file("passs/check".to_string());
         let data = encrypt_thats_all("TRUE".as_bytes().to_vec(), key.clone());
@@ -37,7 +32,7 @@ fn get_pass(first : bool) -> Vec<u8>{
     }
     else{
         println!("Print your master password, please");
-        std::io::stdin().read_line(&mut pass).unwrap();
+        let pass = rpassword::read_password().unwrap();
         let key = pass::get_hash_from_pass(pass.as_bytes());
         if check_pass(key.clone()){
             return key;
