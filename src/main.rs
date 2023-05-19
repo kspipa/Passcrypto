351a3ef5cc5679b45272ba1d51fe70697050a355
kspipa <yyyyyyy5yyyy@gmail.com>
2023-05-19 14:04:59 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ fn main(){
     if file::check_file(format!("{}/log",file::get_path_to_passs())){
         file::rmfile(format!("{}/log", file::get_path_to_passs()));
     }
-    file::create_new_file(format!("{}/log", file::get_path_to_passs()));
-    file::write_to_log("Start");
     if !file::check_file(format!("{}/checkpass", file::get_path_to_passs())){
+        file::mkdir(file::get_path_to_passs());
+        file::create_new_file(format!("{}/log", file::get_path_to_passs()));
         start(&mut siv, true);
     }
     else{
+        file::create_new_file(format!("{}/log", file::get_path_to_passs()));
         start(&mut siv, false);
     }
     siv.run();
 }
 fn get_pass(key : Vec<u8>){
     file::write_to_log("fn : get_pass");
-    file::mkdir(file::get_path_to_passs());
     let path = format!("{}/checkpass", file::get_path_to_passs());
     file::create_new_file(path.clone());
     let data = encrypt_thats_all("TRUE".as_bytes().to_vec(), key.clone());
@@ -94,6 +94,7 @@ fn start(siv : &mut Cursive, first_or_not : bool) {
         file::write_to_log("First time");
     }
     else {
+        file::write_to_log("Start");
         mes = "Print your password".to_string();
         file::write_to_log("Not first time");
     }
@@ -140,15 +141,16 @@ fn right(ui : &mut Cursive, passs : &mut HashMap<Vec<u8>, i8>, key : Vec<u8>){
     let select = SelectView::<String>::new().on_submit( move |x: &mut Cursive , c : &str| {get_compass(x, _jj.clone())}).with_name("select").fixed_size((50, 5));
     let dialog = Dialog::around(LinearLayout::vertical()
         .child(ResizedView::with_fixed_size((5, 2), Button::new("Write new", move |g: &mut Cursive| {get_compass(g, hh.clone());})))
-        .child(ResizedView::with_fixed_size((5, 2), Button::new("Delete", move |g| {delete_pass(g, &mut _ll.clone(), key.clone())})))
+        .child(ResizedView::with_fixed_size((5, 2), Button::new("Delete", move |g| {delete_pass(g, key.clone())})))
         .child(ResizedView::with_fixed_size((5, 2), Button::new("Quit", |g| {g.pop_layer();})))).fixed_size((50, 100));
     ui.add_layer(Dialog::around(LinearLayout::horizontal().child(Dialog::around(select)).child(DummyView.fixed_size((125, 5))).child(dialog)).fixed_size((200, 100)));
     for i in newpasss{
         add_passwords_in_list(pass::from_vec_to_string(decrypt_thats_all(i.0, newkey.clone())), ui);
     }
 }
-fn delete_pass(s: &mut Cursive, passs : &mut HashMap<Vec<u8>, i8>, key : Vec<u8>) {
+fn delete_pass(s: &mut Cursive, key : Vec<u8>) {
     file::write_to_log("fn : delete_pass");
+    let mut passs = s.with_user_data(|hash :  &mut HashMap<Vec<u8>, i8>| {return hash.clone();}).unwrap();
     let mut select = s.find_name::<SelectView<String>>("select").unwrap();
     let _ii = select.selection();
     match select.selected_id() {
@@ -158,13 +160,14 @@ fn delete_pass(s: &mut Cursive, passs : &mut HashMap<Vec<u8>, i8>, key : Vec<u8>
             let encryptedpass = aes256::concat_from_blocks_to_arr(encrypt_thats_all(_ii.clone().unwrap().as_bytes().to_vec(), key.clone()));
             file::write_to_log(format!("data : {:?}", encryptedpass.clone()).as_str());
             let _num = match passs.get(&((encryptedpass))){
-                Some(l) => {s.add_layer(Dialog::info(format!("find id : {l}"))); *l},
-                None => {s.add_layer(Dialog::info(format!("Not find id : {:?}", passs.clone().into_values()))); 120}
+                Some(l) => {*l},
+                None => {120}
             };
             match passs.remove(&(encryptedpass)){
                 Some(_) => file::write_to_log("result : 1"),
                 None => file::write_to_log("result : 0")
             };
+            s.set_user_data(passs);
             if _num == 120{
                 return;
             }
